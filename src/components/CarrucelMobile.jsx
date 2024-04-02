444b12d6f599dc628af3b85e56264dac7492dd8b
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-04-02 17:08:44 -0400

    Update CarrucelMobile.jsx

--- src/components/CarrucelMobile.jsx
+++ src/components/CarrucelMobile.jsx
@@ -25,24 +25,34 @@ function CarrucelMobile() {
     <>
         <section className="relative h-[19rem] md:h-[30rem] ">
 
-            <button onClick={handlePrevImg} className='group absolute left-4 my-auto inset-y-0 grid place-content-center bg-white h-10 w-10 rounded-full'>
+            <button
+                aria-label="Click to see previous image"
+                onClick={handlePrevImg} 
+                className='group absolute left-4 my-auto inset-y-0 grid place-content-center bg-white h-10 w-10 rounded-full'
+            >
                 <svg className='mr-1 stroke-[#1D2026] group-hover:stroke-Orange' width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 1 3 9l8 8" strokeWidth="3" fill="none" fillRule="evenodd"/>
+                    <path d="M11 1 3 9l8 8" strokeWidth="3" fill="none" fillRule="evenodd"/>
                 </svg>
             </button>
+            
             {images.map((item, index) => (
                 <div key={index}>
                     {index === current && 
                     <img 
                         className='object-cover h-[19rem] md:rounded-xl md:h-[30rem] w-full transition-all' 
                         src={item.url}
-                        alt="" 
+                        alt="product" 
                     />}
                 </div>
             ))}
-            <button onClick={handleNextImg} className='group absolute right-4 my-auto inset-y-0 grid place-content-center bg-white h-10 w-10 rounded-full'>
+            
+            <button
+                aria-label="Click to see next image"
+                onClick={handleNextImg} 
+                className='group absolute right-4 my-auto inset-y-0 grid place-content-center bg-white h-10 w-10 rounded-full'
+            >
                 <svg className='ml-1 stroke-[#1D2026] group-hover:stroke-Orange' width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-                <path d="m2 1 8 8-8 8" strokeWidth="3" fill="none" fillRule="evenodd"/>
+                    <path d="m2 1 8 8-8 8" strokeWidth="3" fill="none" fillRule="evenodd"/>
                 </svg>
             </button>
 
