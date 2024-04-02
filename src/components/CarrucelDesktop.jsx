f1ca4a13437c6896d49c00e906d2e2a999cbaedc
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-04-02 17:06:14 -0400

    Update CarrucelDesktop.jsx

--- src/components/CarrucelDesktop.jsx
+++ src/components/CarrucelDesktop.jsx
@@ -25,18 +25,19 @@ function CarrucelDesktop(props) {
         <section className="flex flex-col items-center">
 
             <div className="h-[26rem] w-[26rem] mb-5">
-                <button onClick={props.showLightbox}>
+                <button onClick={props.showLightbox} aria-label="Click to show images on a lightbox" >
                     <img
                         className="rounded-2xl object-fit h-full w-full" 
                         src={images[selected].url} 
-                        alt="" 
+                        alt="Product Image" 
                     />
                 </button>
             </div>
 
             <div className="flex justify-between gap-x-4">
                 {images.map((item, index) => (
-                    <button  
+                    <button
+                        aria-label="Click to change image displayed"
                         key={index} 
                         onClick={() => handleClick(index)} 
                         className={`${index == selected ? 'border-2 border-Orange rounded-2xl' : '' }`}
@@ -44,7 +45,7 @@ function CarrucelDesktop(props) {
                         <img 
                             className={`${styles.base} ${index == selected ? 'opacity-40' : '' }`}
                             src={item.url}
-                            alt="" 
+                            alt="images" 
                         />
                     </button>
                 ))}
