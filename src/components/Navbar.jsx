2966bfe30103a97536560fbfd535384e64fd4ea9
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-04-03 11:37:18 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,9 +67,17 @@ function Navbar(props) {
         <div className='relative flex gap-x-5 lg:gap-x-10 items-center w-full justify-end'>
             <button onClick={showCartitems} aria-label='Shopping cart'>
             { props.isCartEmpty ? '' :
-                <div className='absolute top-[-20%] lg:top-0 lg:right-[4.5rem] right-11 bg-Orange h-5 w-5 text-white font-bold rounded-full grid place-content-center text-[.6rem] lg:text-xs'>
+            <div>
+                <span className={
+                    `${props.animation && 'animate-ping animate-once'} 
+                    absolute top-[-10%] lg:top-1 lg:right-[5rem] right-12 bg-orange-300 opacity-75 h-4 w-4 rounded-full`
+                    }></span>
+                <div
+                className='absolute top-[-10%] lg:top-1 lg:right-[5rem] right-12 bg-Orange h-4 w-4 text-white 
+                font-bold rounded-full grid place-content-center text-[.6rem] xl:text-xs'>
                     {props.cuantity} 
                 </div>
+            </div>
             }
                 <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" 
@@ -142,6 +150,7 @@ function Navbar(props) {
 }
 
 Navbar.propTypes = {
+    animation: PropTypes.bool,
     showbadge: PropTypes.bool,
     itemName: PropTypes.string,
     discountPrice: PropTypes.string,
